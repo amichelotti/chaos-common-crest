fab6d990389bd05d66f2b4761e1a5b470cdfd10a
amichelo <andrea.michelotti@lnf.infn.it>
2018-11-22 12:24:46 +0100

    het prototype

--- het/src/hetAcquire.c
+++ het/src/hetAcquire.c
@@ -1,14 +1,11 @@
+
 /* vmedisp.c, 18/4/96 Paolo Branchini */
 char Usage[] = "\
-Usage: ./provalib.exe [options] address data ... \n\
-       -w##    write to address \n\
-       -r##    read from address \n\
-       -b##    byte access type \n\
-       -s##    shortword access type \n\
-       -l##    longword access type \n\
-       -n##    number of words to read/write \n\
-Example: ./provalib.exe '-w30000000' '-l12345678' '-z9' '-n40' \n\
-";
+Usage: %s \
+       -s <chaos server> \n\
+       -n <CU name> \n\
+       [-a <hetbaseaddress>] \n\
+       ";
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -17,9 +14,15 @@ Example: ./provalib.exe '-w30000000' '-l12345678' '-z9' '-n40' \n\
 #include <string.h>
 #include <errno.h>
 #include <sys/ioctl.h>
-#include <vmedrv.h>
-#include <sys/mman.h>
+#include <string.h>
+#ifdef MOTOROLA
 #include "Vme.h"
+#include <vmedrv.h>
+#endif
+#include <sys/mman.h>
+
+#include "chaos_crest.h"
+
 
 static  unsigned int *vme_ptri,*org_ptr;
 static  unsigned int *vme_ptr1,*org_ptr1;
@@ -28,15 +31,34 @@ static  unsigned short *vme_ptrs,*vme_ptrs1;
 static  unsigned char *vme_ptrc,*vme_ptrc1;
 void MyExceptionHandler(int);
  
+
+DEFINE_CU_DATASET(het_cu)
+DEFINE_ATTRIBUTE("config","configuration channel",DIR_INPUT,TYPE_INT32,sizeof(int32_t))
+DEFINE_ATTRIBUTE("channel0","1 channel",DIR_OUTPUT,TYPE_INT32,sizeof(int32_t))
+DEFINE_ATTRIBUTE("channel1","2 channel",DIR_OUTPUT,TYPE_INT64,sizeof(int64_t))
+DEFINE_ATTRIBUTE("channel2_double","3 channel",DIR_OUTPUT,TYPE_DOUBLE,sizeof(double))
+//DEFINE_ATTRIBUTE("channel3_stringa","4 channel",DIR_OUTPUT,TYPE_STRING,128)
+END_CU_DATASET;        
+
 main(int argc,char *argv[]) {
-  unsigned int address,address1;
+  char chaosserver[64];
+  char cuname[64];
+  int cnt,ret;
+  chaos_crest_handle_t handle;
+  uint32_t cu0;
+  int32_t idata32=0;
+  int64_t idata64=0;
+  double fdata=0;
+   
+  unsigned int address;
   int setvar, setacc, prova_check=0, status,err;
   /* unsigned long dataint,readint,databuf;*/
   unsigned int dataint,readint,databuf;
   unsigned short datashort,readword;
   unsigned char datachar,readchar;
-  int ii, number_of_words,size,cid_pio, cid_pio1,signal_nr, adm;
-  
+  int ii, number_of_words,size,het_fd, het_fd1,signal_nr, adm;
+  *chaosserver=0;
+  *cuname=0;
   address = 0;
   datachar = 0;
   datashort = 0;
@@ -45,75 +67,100 @@ main(int argc,char *argv[]) {
   adm = 0;
   
   if (argc < 3) {
-    fprintf(stderr,Usage);
-    exit(-1);
+    printf("## you must provide a valid chaos server address and a cuname\n");
+    
+    return -1;
   }
   
   argv++; argc--;
-  while (argc>0&&argv[0][0] =='-') {
-    switch (argv[0][1]){
-    case 'r':
-      address = strtoul(&argv[0][2], (char**)NULL, 16);
-      printf("opzione r : address = %08x \n",address);
-      setvar = 0;
-      break;
-    case 'w':
-      address = strtoul(&argv[0][2], (char**)NULL, 16);
-      printf("opzione w : address = %08x \n",address);
-      setvar = 1;
-      break;
-    case 'b':
-      setacc = 1;
-      if(setvar == 1) {
-	datachar = (unsigned char)strtoul(&argv[0][2], (char**)NULL, 16);
-      }
-      printf("opzione b : datachar = %02x \n",datachar);
-      break;
-    case 's':
-      setacc = 2;
-      if(setvar == 1) {
-	datashort = (unsigned short)strtoul(&argv[0][2], (char**)NULL, 16);
-      }
-      printf("opzione s : datashort = %04x \n",datashort);
-      break;
-    case 'l':
-      setacc = 3;
-      if(setvar == 1) {
-	dataint = strtoul(&argv[0][2],(char**) NULL,16);
-      }
-      printf("opzione l : dataint = %08x \n",dataint);
-      break;
-    case 'n':
-      number_of_words = strtoul (&argv[0][2],(char**) NULL,16);
-      printf("opzione n : number_of_words = %d \n",number_of_words);
-      break;
-    case 'z':
-      adm =             strtoul (&argv[0][2],(char**) NULL,16);
-      printf("opzione z : adc = %08x \n",adm);
-      break;
+  while (cnt<argc) {
+    if((!strcmp(argv[cnt],"-s"))&&((cnt+1)<argc)){
+      strncpy(chaosserver,sizeof(chaosserver),argv[++cnt]);
+      continue;
     }
-    argv++;argc--;
+    if((!strcmp(argv[cnt],"-n"))&&((cnt+1)<argc)){
+      strncpy(cuname,sizeof(cuname),argv[++cnt]);
+      continue;
+    }
+    if((!strcmp(argv[cnt],"-a"))&&((cnt+1)<argc)){
+      address=strtoul(argv[++cnt]);
+      continue;
+    }
+    if((!strcmp(argv[cnt],"-h"))){
+      fprintf(stdout,Usage,argv[0]);
+      return 0;
+    }
   }
+   
+   if(*chaosserver==0){
+         printf("## you must provide a valid metadaserver address\n");
+      return -2;
+   }
+   if(*cuname==0){
+         printf("## you must provide a valid cuname\n");
+      return -3;
+   }
   
-  printf("address = %08x \n",address);
-  printf("datashort = %04x \n",datashort);
-  printf("datachar = %02x \n",datachar);
-  printf("dataint = %08x \n",dataint);
-  printf("number_of_words = %08x \n",number_of_words);
-  printf("adm = %08x \n",adm); 
+  printf("chaosserver = %s \n",chaosserver);
+  printf("cuname = %s \n",cuname);
+  printf("HET VME ADDRESS = %08x \n",address);
+ #ifdef MOTOROLA
+  het_fd = VmeOpenChannel("het", "het");
+  VmeSetExceptionHandling(Vme_EXCEPTION_EXIT);
+  VmeSetProperty(het_fd, Vme_SET_DTYPE, 0);
+  org_ptr =  (unsigned int *)VmeMapAddress(het_fd, 0xffff0000&address, 0x1000000, adm);
+ #endif
+ 
+  handle=chaos_crest_open(chaosserver);
+  if(handle==NULL){
+        printf("## cannot open crest interface for url:%s\n",chaosserver);
+        return -3;
+    }
+    cu0=ADD_CU(handle,cuname,het_cu);
+    if(cu0==0){
+        printf("## failed adding cu \"%s\"\n",cuname);
+        return -4;
+    }
+    printf("* registering to %s...\n",chaosserver);
+    // 0 means all defined CU
+    if((ret=chaos_crest_register(handle,0))!=0){
+      printf("## cannot register CUs, error:%d\n",ret);
+      return -9;
+    }
+    printf("* registration average  %f ms...\n",chaos_crest_reg_time(handle));
+    printf("* pushing to %s...\n",chaosserver);
+    cnt=0;
+    while(1){
+       idata32++;
+        idata64+=2;
+        fdata=3.14*(++cnt);
+        //positional attribute, 0 is the first output attribute of cu0
+        chaos_crest_update(handle,cu0,0,&idata32);
+	
+        chaos_crest_update(handle,cu0,1,(void*)&idata64);
+        chaos_crest_update(handle,cu0,2,&fdata);
+	//        sprintf(sdata,"test stringa %d",idata32);
+	//        chaos_crest_update(handle,cu0,3,sdata);
+        idata32++;
+        idata64+=2;
+        fdata=3.14*cnt;
+        if((ret=chaos_crest_push(handle,0))!=0){
+	        printf("## error pushing ret:%d\n",ret);
+          return -4;
+        }
+    }
+    chaos_crest_close(handle);
+    return (0);
   /* 
      adm = 0x9  : A32 
            0x39 : A24
            0x29 : A16    
   */
-  cid_pio = VmeOpenChannel("prova", "pio");
-  VmeSetExceptionHandling(Vme_EXCEPTION_EXIT);
-  VmeSetProperty(cid_pio, Vme_SET_DTYPE, 0);
-  org_ptr =  (unsigned int *)VmeMapAddress(cid_pio, 0xffff0000&address, 0x1000000, adm);
-  vme_ptri = (unsigned int *) ((long)org_ptr + (address&0xffff));  /* D32 */
-  vme_ptrs = (unsigned short*)((long)org_ptr + (address&0xffff));  /* D16 */
-  vme_ptrc = (unsigned char*) ((unsigned int)org_ptr + ((address)&0xffff));  /* D8 */
  
+  //vme_ptri = (unsigned int *) ((long)org_ptr + (address&0xffff));  /* D32 */
+  //vme_ptrs = (unsigned short*)((long)org_ptr + (address&0xffff));  /* D16 */
+  //vme_ptrc = (unsigned char*) ((unsigned int)org_ptr + ((address)&0xffff));  /* D8 */
+ /*
   switch (setvar) {
   case 0:
     switch (setacc) {
@@ -165,8 +212,10 @@ main(int argc,char *argv[]) {
       break;
     }
   }
-  
-  VmeCloseChannel(cid_pio);
-  exit(0);
+  */
+ #ifdef MOTOROLA
+  VmeCloseChannel(het_fd);
+#endif
+return 0;
 }
 
