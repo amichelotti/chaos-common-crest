e576d0438b0cd78754f1e8b788186245c895e231
amichelo <andrea.michelotti@lnf.infn.it>
2018-12-10 17:58:15 +0100

    updates

--- src/chaos_crest.c
+++ src/chaos_crest.c
@@ -291,8 +291,7 @@ static unsigned long long getEpoch()
   struct timeval mytime;
   uint64_t ret;
   gettimeofday(&mytime, NULL);
-  ret = mytime.tv_sec;
-  ret = ret * 1000 + mytime.tv_usec / 1000;
+  ret = mytime.tv_sec * 1000 + (mytime.tv_usec / 1000);
 
   return ret;
 }
